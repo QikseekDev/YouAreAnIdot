fed0d434c8bbb673c07e648db48a41dac0838cc5
Vojtěch Vokoun <vokounvojta03@gmail.com>
2026-06-16 20:39:58 +0200

    Refactor openWindow function
    Refactor openWindow function to improve readability and maintainability.

--- scripts/app.js
+++ scripts/app.js
@@ -35,34 +35,38 @@ document.addEventListener("mousemove", e => {
     mouseX = e.clientX;  
     mouseY = e.clientY;  
 });  
-function openWindow(url) {  
-    const width = 357;  
-    const height = 330;  
-    const left = Math.floor((screen.width - width) / 2);  
-    const top = Math.floor((screen.height - height) / 2);  
-  
-    const features = `menubar=no,status=no,toolbar=no,resizable=no,  
-        width=${width},height=${height},left=${left},top=${top},  
-        noopener,noreferrer`;  
-  
-    const aWindow = window.open(url, "_blank", features);  
-  
-    if (aWindow) {  
-        openWindows.push(aWindow);  
-  
-        const timer = setInterval(() => {  
-            for (let i = openWindows.length - 1; i >= 0; i--) {  
-                if (openWindows[i].closed) openWindows.splice(i, 1);  
-            }  
-  
-            if (aWindow.closed) {  
-                clearInterval(timer);  
-                proCreate(); 
-                }  
-            }  
-        }, 40);  
-    }  
-}  
+function openWindow(url) {
+    const width = 357;
+    const height = 330;
+
+    const left = Math.floor((screen.width - width) / 2);
+    const top = Math.floor((screen.height - height) / 2);
+
+    const features =
+        `menubar=no,status=no,toolbar=no,resizable=no,` +
+        `width=${width},height=${height},left=${left},top=${top},` +
+        `noopener,noreferrer`;
+
+    const aWindow = window.open(url, "_blank", features);
+
+    if (aWindow) {
+        openWindows.push(aWindow);
+
+        const timer = setInterval(() => {
+            for (let i = openWindows.length - 1; i >= 0; i--) {
+                if (openWindows[i].closed) {
+                    openWindows.splice(i, 1);
+                }
+            }
+
+            if (aWindow.closed) {
+                clearInterval(timer);
+                proCreate();
+            }
+
+        }, 40);
+    }
+} 
   
 function proCreate() {  
     for (let i = 0; i < 4; i++) {  
